4319d52bfb33adfa8fe2eaaada2856d894bff8c8
AndruMace <amacedeveloper@gmail.com>
2025-02-16 20:56:47 -0600

    make alert more obvious and follow player once alerted

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,8 +1,8 @@
 class_name Enemy extends CharacterBody3D
 @export var point_a: Vector3
 @export var point_b: Vector3
-@export var speed: float = 5.0
-
+@export var speed: float = 2.0
+@export var player: RigidBody3D
 var alerted: bool = false
 var target: Vector3
 
@@ -13,16 +13,18 @@ func _ready():
 func _physics_process(delta):
 	# Calculate the direction vector from the enemy to the target
 	var direction = (target - position).normalized()
-	
+	position += direction * speed * delta
 	if (!alerted):
 		alerted = can_see_player()
-	# Move the enemy towards the target
-	position += direction * speed * delta
-	# Check if we've reached (or are close enough to) the target
-	if position.distance_to(target) < 1.0:
-		# Switch target between point A and point B
-		target = point_a if target == point_b else point_b
+		# Check if we've reached (or are close enough to) the target
+		if position.distance_to(target) < 1.0:
+			# Switch target between point A and point B
+			target = point_a if target == point_b else point_b
+			look_at(target, Vector3.UP)
+	else:
+		target = Vector3(player.position.x, position.y, player.position.z)
 		look_at(target, Vector3.UP)
+
 
 func can_see_player() -> bool:
 	for child in $Rays.get_children():
@@ -32,5 +34,13 @@ func can_see_player() -> bool:
 				var collider = child.get_collider()
 				 # Check by comparing against the player’s group or name
 				if collider and collider.is_in_group("Player"):
+					set_alerted()
 					return true
 	return false
+
+func set_alerted():
+	speed += 2
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = Color.RED
+	$MeshInstance3D.set_surface_override_material(0, mat)
+	
