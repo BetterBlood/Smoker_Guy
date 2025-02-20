7cbb24dea72304f371bb42dc2d11aad2bdf19b31
Better_Blood <better_blood@hotmail.com>
2025-02-20 20:11:20 +0100

    Better sound Emitter

--- scripts/sound_emitter.gd
+++ scripts/sound_emitter.gd
@@ -3,12 +3,14 @@ extends Node3D
 @onready var radar: Area3D = $Emitter
 
 var strength: int
+var propagation:= Vector3(0.5, 0.5, 0.5)
+var propagation_speed:= 10
 
 func setStrengh(stren:= 50) -> void:
 	strength = stren
 
 func _process(delta: float) -> void:
 	if radar.scale.x < strength:
-		radar.scale *= 2
+		radar.scale += propagation * propagation_speed
 	else:
 		queue_free()
